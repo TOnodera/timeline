6a23e89a0e22c5931b24eec430e640eab4fb74f7
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-04-29 16:37:30 +0000

    修正

--- timeline/resources/config/index.js
+++ timeline/resources/config/index.js
@@ -1,4 +1,4 @@
-requeire('dotenv').config();
+require('dotenv').config();
 const baseUrl = process.env.NODE_ENV === "prod" ? process.env.WEB_SERVER_URL : "http://localhost:3000";
 
 export { baseUrl };
