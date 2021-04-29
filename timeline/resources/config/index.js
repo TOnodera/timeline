dad2f069293e97def0f0f1a54be13ae1abd8df74
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-04-29 09:14:59 +0000

    dockercomposeで読むように変更

--- timeline/resources/config/index.js
+++ timeline/resources/config/index.js
@@ -1,3 +1,3 @@
-const baseUrl = "http://localhost:3000";
+const baseUrl = "http://twittersse:3000";
 
 export { baseUrl };
--- timeline/resources/js/components/Timelines.vue
+++ timeline/resources/js/components/Timelines.vue
@@ -36,6 +36,7 @@ export default {
         source.onerror = (e) => {
             console.log(`error:`,e);
         }
+        
     }
 }
 </script>
