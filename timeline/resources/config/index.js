3e3cf7d124f594ab2bb862d502dca09cff26b822
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-04-29 16:23:13 +0000

    修正

--- timeline/resources/config/index.js
+++ timeline/resources/config/index.js
@@ -1,3 +1,3 @@
-const baseUrl = process.env.NODE_ENV === "prod" ? "http://timeliner.tk:3000" : "http://localhost:3000";
+const baseUrl = process.env.NODE_ENV === "prod" ? process.env.WEB_SERVER_URL : "http://localhost:3000";
 
 export { baseUrl };
